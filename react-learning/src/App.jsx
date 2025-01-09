6684472e4c918ca7fbd2c206c1ea478d8739923c
Morlok <nikitaillarionov2018@gmail.com>
2025-01-09 17:42:07 +0200

    Timeline: 3.01.38 added Updating of the already existing jobs, PUT method.

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -15,9 +15,10 @@ import { //added
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage' ;
+import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 
 const App = () => {
@@ -31,8 +32,8 @@ const App = () => {
       },
       body: JSON.stringify(newJob),
     });
-      
-      return;
+
+    return;
   };
   //delete job 
   const deleteJob = async (id) => {
@@ -40,37 +41,51 @@ const App = () => {
     const res = await fetch(`/api/jobs/${id}`, {
       method: 'DELETE',
     });
-      
-      return;
+
+    return;
   };
-  
+
+  //update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job),
+    });
+
+    return;
+  }
+
   const router = createBrowserRouter(//added
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-  
-        <Route index element ={<HomePage/>} />
-        <Route path='/jobs' element ={<JobsPage/>} />
-        <Route path='/add-job' element ={<AddJobPage  addJobSubmit={addJob}/>} />
-        <Route path='/jobs/:id' element ={<JobPage  deleteJob={deleteJob}/>} loader ={jobLoader} />                  {/*  :id is a dynamic parameter  */ }
-        
-        <Route path='*' element ={<NotFoundPage/>} />
-  
+
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />                  {/*  :id is a dynamic parameter  */}
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+
+        <Route path='*' element={<NotFoundPage />} />
+
       </Route>
-      
-  ))
+
+    ))
   return <RouterProvider router={router} />
 
 
 
-   /*{/* <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JonListings />
-    <ViewAllJobs /> 
-    </> */
+  /*{/* <>
+   <Navbar />
+   <Hero />
+   <HomeCards />
+   <JonListings />
+   <ViewAllJobs /> 
+   </> */
   //}
-  
+
 }
 
 export default App;
